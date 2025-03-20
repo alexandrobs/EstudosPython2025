c29a9a733336a95086e1a90bed16fe0838b67af3
Alexandro B S <jornalsinal@gmail.com>
2025-03-19 21:04:34 -0300

    Blocos, Instruções e Expressões

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,6 @@
 print("Hello, World!")
 
-
-# a importância da indentação em python e o correto número de tabulação para padrão de código e evitar o erro IndentationError
+# A importância da indentação em python e o correto número de tabulação para padrão de código e evitar o erro IndentationError
 
 x = 15
 y = 100
@@ -9,3 +8,20 @@ if x > y:
     print("x é maior que y")
 else:
     print("x é menor que y")
+
+# Expressões, instruções, blocos e tipo da linguagem
+x = 15
+print("x: " + str(x))
+
+y = (x + 10) / 100
+if x > y:
+    print("x: "+ str(x) + " | y: " + str(y))
+
+passei = False
+if passei:
+    print("Churrasco garantido!")
+else:
+    print("Não foi dessa vez.")
+    print("É aprender com os erros e continuar!")
+
+# Blocos contém instruções que por sua vez contém expressões, que criam, manipulam e exibem objetos e variáveis
